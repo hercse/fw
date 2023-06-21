9a4eee3d420f0d98285bdb4d252540d1063f5ed9
Hercse <xw12025@gmail.com>
2023-06-21 14:50:16 +0800

    Add files via upload

---
+++ fw.js
@@ -0,0 +1,44 @@
+function generateFrame(frame) {
+  // Header HTML template
+  const headerTemplate = `
+      <div class="header">
+        <div class="src">
+          <span><a href="http://herc.se/">[herc.se]</a> / </span>${frame.name}
+          / ${frame.header.src}
+        </div>
+        <div class="tool">
+          ${frame.header.menu}
+        </div>
+      </div>`;
+
+  // Footer HTML template
+  const footerTemplate = `
+      <div class="footer">
+        <div class="display">
+          <div class="info">
+            <label for="">[!] <input type="checkbox" name="" id="" /></label>
+          </div>
+          <div class="copyright">
+            Copyright © 2022 Hercse. All rights reserved.
+          </div>
+        </div>
+        <div class="about">
+          ${frame.Info}
+        </div>
+      </div>`;
+
+  // Insert the generated HTML into the page
+  document.body.innerHTML += headerTemplate + footerTemplate;
+}
+
+function addStyles(css) {
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
+const styles = `
+.header{z-index:100;position:fixed;top:0;left:0;padding:calc(var(--dot)*1);font-weight:normal;background-color:#000;width:100%;display:grid;grid-template-columns:repeat(4, 1fr)}.header span{opacity:.3}.header .src{grid-column-end:span 3}.header .tool{text-align:right}.header .tool>div{display:inline-block}.footer{z-index:100;position:fixed;bottom:0;left:0;padding:calc(var(--dot)*1);font-weight:normal;background-color:#000;width:100%;transition:transform 1s}.footer:has(input:checked){border-top:1px solid #fff}.footer:has(input:checked) .about{display:block}.footer .about{margin-top:calc(var(--dot)*10);max-height:50vh;height:calc(var(--dot)*40);overflow-y:scroll;display:none}.footer .about p{margin-bottom:0;max-width:calc(var(--dot)*100)}.footer .display{display:grid;grid-template-columns:repeat(2, 1fr)}.footer .display .info{position:relative}.footer .display .info input{left:0;top:0;height:100%;width:100%}.footer .display .copyright{text-align:right}/*# sourceMappingURL=frame.min.css.map */
+`;
+
+addStyles(styles);
